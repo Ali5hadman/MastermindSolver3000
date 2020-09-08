68590b16f6ff82b0139f39f7a866d528e2ebd6d1
AlishadmanORB <ali@orb.group>
2020-09-09 01:37:52 +0200

    Made game w states

---
+++ ThesisTryOutpy/main.py
@@ -0,0 +1,110 @@
+import random
+import array
+
+def getcode():
+    code=[]
+    for i in range(4):
+        code.append(random.randint(1,6))
+    return code
+
+def checkcode(code,guess):
+    result =[]
+    for i in range (4):
+            if guess[i] == code[i]:
+                result.append('B')
+                code[i] *= -1
+                guess[i] *= -1
+    for i in range(4):
+        if guess[i]>0:
+             for j in range(4):
+                    if guess[i]==code[j]:
+                        result.append('W')
+                        code[j]*=-1
+    for i in range(4):
+        if guess[i]<0:
+            guess[i]*=-1
+        if code[i]<0:
+            code[i]*=-1
+    return result
+
+
+def createset():
+    state=[]
+    for x in range(1, 7):
+        for y in range(1, 7):
+            for z in range(1, 7):
+                for q in range(1, 7):
+                    state.append([x, y, z, q])
+    x=len(state)
+    print(x)
+    return state
+
+def tempcheckcode(code,guess):
+    tempresult=[]
+    for i in range (4):
+            if guess[i] == code[i]:
+                tempresult.append('B')
+                code[i] *= -1
+                guess[i] *= -1
+    for i in range(4):
+        if guess[i]>0:
+             for j in range(4):
+                    if guess[i]==code[j]:
+                        tempresult.append('W')
+                        code[j]*=-1
+    for i in range(4):
+        if guess[i]<0:
+            guess[i]*=-1
+        if code[i]<0:
+            code[i]*=-1
+    return tempresult
+
+
+
+state=createset()
+code= getcode()
+guess = [1,1,2,2]
+c=0
+won=False
+while won==False:
+    c=c+1
+    print("Turn"+str(c)+" ======================================")
+    print("code = "+str(code))
+    print("guess  = " + str(guess))
+    result=checkcode(code,guess)
+    print("result = "   + str(result))
+
+    for i in state:
+      tempresult =tempcheckcode(guess,i)
+      if tempresult != result:
+          state.remove(i)
+      tempresult.clear()
+    guess=state[0]
+    print("len of state = "+str(len(state)))
+    print("current state = "+ str(state))
+    if result == ['B','B','B','B']:
+        won=True
+    result.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
